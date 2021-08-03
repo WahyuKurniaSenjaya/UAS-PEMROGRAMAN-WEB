6ad2e4ef7168e67c4734296c1531d3e8116f4ab6
WahyuKurniaSenjaya <87650735+WahyuKurniaSenjaya@users.noreply.github.com>
2021-08-03 21:51:56 +0700

    create

---
+++ Program Penggajian/jav2.js
@@ -0,0 +1,19 @@
+    function proses(){
+ 
+    // Deklarasi
+    var jk=document.getElementById("jk").value;
+    var up=document.getElementById("up").value;
+    var a=document.getElementById("a").value;
+
+    // menghitung Gaji karyawan
+    var g=jk*up;
+    document.getElementById("g").value=g;
+
+    // menghitung Tunjangan anak 10%
+    var ta=g*.1*a;
+    document.getElementById("ta").value=ta;
+
+    // menghitung Total Gaji
+    var tg=g+ta;
+    document.getElementById("tg").value=tg
+}
